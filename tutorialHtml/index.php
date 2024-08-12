6f90f7a451dc90d9f6266f487fcc64daf356e126
Xennox117 <jesusdarodriguez23@gmail.com>
2024-08-12 15:29:01 -0500

    primeraVersion

---
+++ tutorialHtml/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html> 
+
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial de HTML y CSS</title>
+    <link rel="stylesheet" href="css/newcss.css">
+</head>
+<body>
+    <header>
+        <h1>Componentes de HTML y CSS</h1>
+    </header>
+    <nav>
+        <ol>
+            <li>
+                <a href="#html">HTML</a>
+                <ul class="submenu">
+                    <li><a href="etiquetasDeHtml.html">Etiquetas</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#css">CSS</a>
+                <ul class="submenu">
+                    <li><a href="etiquetasDeCss.html">Etiquetas</a></li>
+                    <li><a href="estilosDeCss.html">Estilos</a></li>
+                </ul>
+            </li>
+        </ol>
+    </nav>                
+
+      
+    <main>
+        <section id="html">
+            <h2>HTML</h2>
+            <p>HTML (HyperText Markup Language) es el lenguaje estándar para crear páginas web. Consiste en una serie de elementos que se utilizan para estructurar y dar formato al contenido.</p>
+            <br/>(IMAGEN DE EJEMPLO)
+            <br/>
+            <img src="img/html.png" alt="Ejemplo de HTML">                      
+        </section>
+        <section id="css">
+            <h2>CSS</h2>
+            <p>CSS (Cascading Style Sheets) es un lenguaje utilizado para describir la presentación de un documento escrito en HTML. Mediante el cual se otorga a una página web su apariencia.</p>
+            <br/>(IMAGEN DE EJEMPLO)
+            <br/>
+            <img src="img/css.png" alt="Ejemplo de CSS">
+            <br/>
+            <img src="img/css2.png" alt="Ejemplo de CSS">
+            
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 Tutorial de HTML y CSS</p>
+    </footer>
+</body>
+</html>
